8a56b3257e9b20be568832b91c79a1aca75fd461
roy adrian cidro <royadriancidro0@gmail.com>
2023-03-14 10:57:45 +0800

    removed extra statements

---
+++ ceasarCipher.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+
+void encrypt(std::string& m, int r){
+  for(char& c : m){
+    if(std::isalpha(c)){
+      c = std::tolower(c);
+      c = ((c-'a' + r)%26) + 'a';
+
+    }
+  }
+}
+
+int main(){
+  std::string msg {};
+  auto rot {0};
+
+  std::cout << "Your secrect message: ";
+  std::getline(std::cin, msg);
+  std::cout << "Your secret key: ";
+  std::cin >> rot;
+
+  encrypt(msg, rot);
+  std::cout << "The ecoded message:  " << msg << std::endl;
+  
+}
